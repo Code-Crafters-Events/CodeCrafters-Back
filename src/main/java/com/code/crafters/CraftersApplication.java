c2c65328147b2dc4cb47ac9438a6fb21b3f30641
Jennyx-1984 <163904901+Jennyx-1984@users.noreply.github.com>
2026-04-07 10:55:26 +0200

    chore: initial structure

---
+++ src/main/java/com/code/crafters/CraftersApplication.java
@@ -0,0 +1,13 @@
+package com.code.crafters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftersApplication.class, args);
+	}
+
+}
