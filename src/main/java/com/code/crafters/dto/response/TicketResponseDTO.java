fad40abf4df5585e2477396c10cb9fa5227fafbc
Jennyx-1984 <163904901+Jennyx-1984@users.noreply.github.com>
2026-04-19 23:32:59 +0200

    add: new property for verification Code of the QRs in TicketResponseDTO

--- src/main/java/com/code/crafters/dto/response/TicketResponseDTO.java
+++ src/main/java/com/code/crafters/dto/response/TicketResponseDTO.java
@@ -5,15 +5,16 @@ import java.time.LocalDateTime;
 import com.code.crafters.entity.enums.PaymentStatus;
 
 public record TicketResponseDTO(
-        Long id,
-        LocalDateTime createdAt,
-        Long userId,
-        String userName,
-        String userProfileImage,
-        Long eventId,
-        String eventTitle,
-        PaymentStatus paymentStatus,
-        String paymentIntentId,
-        String qrUrl) {
+                Long id,
+                LocalDateTime createdAt,
+                Long userId,
+                String userName,
+                String userProfileImage,
+                Long eventId,
+                String eventTitle,
+                PaymentStatus paymentStatus,
+                String paymentIntentId,
+                String qrUrl,
+                String verificationCode) {
 
 }
